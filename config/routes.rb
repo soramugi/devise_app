a884b2d836b42e6b1bc9fc3ab530ad0fe9333080
soramugi <soramugi.chika@gmail.com>
2014-04-05 17:59:41 +0900

    user create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 DeviseApp::Application.routes.draw do
   root "home#index"
   get "home/index"
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

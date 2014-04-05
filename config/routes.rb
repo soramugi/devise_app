f5e168da50361d2be828c174572321adca726cad
soramugi <soramugi.chika@gmail.com>
2014-04-05 17:11:54 +0900

    add controller home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DeviseApp::Application.routes.draw do
+  root "home#index"
+  get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

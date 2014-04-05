a04b9249d0f8ecd55adea9d5d79e05d1ec39f54e
soramugi <soramugi.chika@gmail.com>
2014-04-05 18:27:15 +0900

    sign in twitter

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ DeviseApp::Application.routes.draw do
   root "home#index"
   get "home/index"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
